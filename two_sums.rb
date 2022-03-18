5c5e294541e8391096270c7b73f4714637bc484e
Amanda Rodgers <aarodgerswork@gmail.com>
2022-03-18 11:59:32 -0600

    adds two_sums

--- two_sums.rb
+++ two_sums.rb
@@ -3,5 +3,20 @@
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 #You can return the answer in any order.
+# @param {Integer[]} nums = []
+# @param {Integer} target = int
+# @return {Integer[]}
 
+# nums = [2, 7, 11, 15]
 
+def two_sum(nums, target)
+  hash = {}
+  nums.each_with_index do |n, i|
+    if hash[target - n]
+      return hash[target - n], i
+    end
+    hash[n] = i
+  end
+end
+
+puts two_sum([2, 7, 11, 15], 9)
