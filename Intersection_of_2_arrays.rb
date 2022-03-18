10fe2cab213cec826b9aecb873a7c7ab0e1e1d1d
Amanda Rodgers <aarodgerswork@gmail.com>
2022-03-18 09:18:07 -0600

    adds intersection of two arrays no repeats

--- Intersection_of_2_arrays.rb
+++ Intersection_of_2_arrays.rb
@@ -1,7 +1,15 @@
 # Given two arrays write a funciton to compute their intersection
 
+require 'set'
+
 nums1 = [4, 9, 5]
 nums2 = [9, 4, 9, 8, 4]
 # Output: [9, 4]
 
+def intersection
+  combine = Set[4, 9, 5] & Set[9, 4, 9, 8, 4]
+  puts combine
+end
 
+intersection
+# => #<Set: {9, 4}>
