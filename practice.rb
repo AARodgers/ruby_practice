5d95b98c4fe85f07aeefc9c190cc1af62c989252
Amanda Rodgers <aarodgerswork@gmail.com>
2022-03-20 21:35:06 -0600

    adds each_char_with_index

--- practice.rb
+++ practice.rb
@@ -1,1 +1,16 @@
+def each_c_with_index
+a = ["flower","flow","flight"]
+  a[0].each_char.with_index do |c, i|
+      puts "Char: #{c}, Index: #{i}"
+  end
+end
 
+puts each_c_with_index
+# returns
+#Char: f, Index: 0
+# Char: l, Index: 1
+# Char: o, Index: 2
+# Char: w, Index: 3
+# Char: e, Index: 4
+# Char: r, Index: 5
+# flower
