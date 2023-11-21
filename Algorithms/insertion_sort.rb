b6dc2cfa5d714b394b0c331e3e24a385d4d1fe7f
Amanda Rodgers <aarodgerswork@gmail.com>
2023-11-21 07:11:45 -0700

    Adds insertion_sort

--- Algorithms/insertion_sort.rb
+++ Algorithms/insertion_sort.rb
@@ -8,6 +8,7 @@ array = [5,2,1,3,4]
 def insertion_sort(array)
   for i in 1...array.length
     j = i
+    # when would j ever be 0?
     while j > 0
       if array[j-1] > array[j]
         temp = array[j]
@@ -16,6 +17,8 @@ def insertion_sort(array)
       else
         break
       end
+      j = j - 1
     end
   end
+  return array
 end
