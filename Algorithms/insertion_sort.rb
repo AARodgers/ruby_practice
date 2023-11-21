abe828a41dcd8364c0674130ba21fbed061639de
Amanda Rodgers <aarodgerswork@gmail.com>
2023-11-21 09:48:48 -0700

    Add debugging notes

--- Algorithms/insertion_sort.rb
+++ Algorithms/insertion_sort.rb
@@ -7,14 +7,21 @@ array = [5,2,1,3,4]
 
 def insertion_sort(array)
   for i in 1...array.length
+    puts
+    puts "sorting unsorted #{i}"
     j = i
     # when would j ever be 0?
     while j > 0
+      puts "comparing #{j-1} (value: #{array[j-1]}) with #{j} (value: #{array[j]})"
       if array[j-1] > array[j]
+        puts "swapping"
         temp = array[j]
-        array[j-1] = array[j]
+        array[j] = array[j-1]
         array[j-1] = temp
+        # array[1] = array[0]
+        # array[0] = array[1]
       else
+        puts "not swapping"
         break
       end
       j = j - 1
@@ -22,3 +29,5 @@ def insertion_sort(array)
   end
   return array
 end
+
+puts insertion_sort(array)
