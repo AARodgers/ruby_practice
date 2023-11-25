1c53c668d534e8e7781a1703835b20811d88c00f
Amanda Rodgers <aarodgerswork@gmail.com>
2023-11-25 08:47:35 -0700

    Changes file names for debugged file and not debugged file

--- Algorithms/insertion_sort.rb
+++ Algorithms/insertion_sort.rb
@@ -1,28 +1,14 @@
-# Pros:
-#  is stable: does not change the relative order of elements with equal keys
-#  is an inplace algorithm, so it does not create a new array
-
-
-array = [5,2,1,3,4]
+array = [4, 5, 1, 3, 2]
 
 def insertion_sort(array)
   for i in 1...array.length
-    puts
-    puts "sorting unsorted #{i}"
     j = i
-    # j would be zero if array was only 1 element
     while j > 0
-      puts "comparing #{j-1} (value: #{array[j-1]}) with #{j} (value: #{array[j]})"
       if array[j-1] > array[j]
-        puts "swapping"
         temp = array[j]
         array[j] = array[j-1]
-        puts "#{array.index(2)}"
         array[j-1] = temp
-        puts "#{array.index(5)}"
-        puts
       else
-        puts "not swapping"
         break
       end
       j = j - 1
@@ -32,3 +18,5 @@ def insertion_sort(array)
 end
 
 puts insertion_sort(array)
+
+
