5865d3029cd5d30c80fd600f6d6fd51675fad17b
Amanda Rodgers <aarodgerswork@gmail.com>
2023-11-28 16:58:57 -0700

    Complete merge_sort2.rb

--- Algorithms/merge_sort2.rb
+++ Algorithms/merge_sort2.rb
@@ -2,21 +2,31 @@
 #  MERGE SORT
 # Big O (n log(n))
 
-list = [23. 82, 7, 5, 12]
-
 def merge_sort(list)
   if list.length <= 1
-    return list
+    list
   else
-    mid = (list.lenth / 2).floor
-    left = merge_sort(list[0..mid -1])
-    right = merge_sort(list[mid..list.length -1])
-
-
+    mid = (list.length / 2).floor
+    left = merge_sort(list[0..mid - 1])
+    right = merge_sort(list[mid..list.length])
+    merge(left, right)
+  end
 end
 
 def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
 end
+
+arr = Array.new(30) { rand(1..150) }
+puts merge_sort(arr)
 
 
 # .floor will round 4.5, 4.2 or 4. 8 all down to 4
